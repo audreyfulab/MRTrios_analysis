7c7dd6a9dd72c199885048bb99439774b69c7458
LianZuo <112709486+LianZuo8197@users.noreply.github.com>
2026-04-30 13:38:00 -0400

    Refactor data loading and filtering in get_trio_data

--- get_trio_data.R
+++ get_trio_data.R
@@ -13,10 +13,14 @@ library(MRGN)
 setwd("/Users/lianzuo/LZ/ResearchProject/Fulab/MRTrios_analysis/raw_Data_Methyl")
 
 # ----- load data files -----
-load("meth.posER.sig.pcs.indices.RData") # pos_meth indice
-gene_pos_indice <- readRDS("gene.exp.posER.sig.asso.pcs.RData")$sig.asso.covs # pos_gene.exp indice
+gene_indice.pos  <- readRDS("gene_exp_posER_sig_pcs_indices.rds") # gene.exp indices values
+meth_indice.pos  <- readRDS("meth_posER_sig_pcs_indices.rds") ## meth PC raw and corrected index
 pc.gene.pos <- fread("PCA.gene.exp.posER.txt")
 pc.meth.pos <- fread("PCA.meth.posER.txt")
+
+### Note:meth,gene.exp table : # V1,col1, col2 , the col2 is the final pcscore index.
+gene.table.pos <- fread("gene.exp.posER.table.txt") ## gene.exp PC raw and corrected index
+meth.table.pos <- fread("meth.posER.table.txt")# meth indices values
 
 cna <- fread('data_CNA.txt',data.table = F)
 gene.exp <- fread('data_RNA_Seq_v2_mRNA_median_all_sample_Zscores.txt',data.table = F)
@@ -37,7 +41,7 @@ clinical.pos  <- fread("names.pos.patient2.txt", header = FALSE) # ER+ patients 
 ## com.ind from 3 datasets:cna,gene.exp, meth
 com.ind = intersect(
   intersect(colnames(gene.exp)[3:ncol(gene.exp)],
-    colnames(TCGA.meth)[5:ncol(TCGA.meth)]),
+            colnames(TCGA.meth)[5:ncol(TCGA.meth)]),
   colnames(cna)[3:ncol(cna)]) # length 777
 
 ## com_id of 4 datasets:cna,gene.exp, meth,clinical 
@@ -60,7 +64,7 @@ pos_exp_filter <-  gene.exp %>% mutate(gene.row= rownames(gene.exp)) %>%
   filter(gene.row %in% unique(trios$gene.row)) %>% 
   select(gene.row,all_of(id_pos_filter))
 
-pos_meth_filter <-  meth %>% mutate(meth.row= rownames(meth)) %>% 
+pos_meth_filter <-  TCGA.meth %>% mutate(meth.row= rownames(TCGA.meth)) %>% 
   filter(meth.row %in% unique(trios$meth.row)) %>% 
   select(meth.row,all_of(id_pos_filter))
 
@@ -82,8 +86,10 @@ get_trio_data <- function(trio_row,
                           pos_exp_filter,
                           pca_E_final,
                           pca_M_final,
-                          gene_pos_indice,
-                          meth.posER.pc.sig,
+                          gene_indice.pos,
+                          meth_indice.pos,
+                          gene.table.pos,
+                          meth.table.pos,
                           pos_age,
                           pos_race) {
   
@@ -98,13 +104,15 @@ get_trio_data <- function(trio_row,
   C <- pos_cna_filter  %>% filter(cna.row  == cna_row)
   E <- pos_exp_filter  %>% filter(gene.row == gene_row)
   
-  # --- PCA covariates for expression ---
-  U_E     <- gene_pos_indice[gene_row]
-  U_exp   <- pca_E_final %>% select(all_of(as.numeric(U_E[[1]])))
+  # Retrieve significant PCs for expression
+  gene_pc_index <- gene.table.pos %>% filter(col1 == gene_row) %>% pull(col2)
+  U_exp <- pca_E_final %>% select(all_of(as.numeric(gene_indice.pos[gene_pc_index][[1]])))
   
-  # --- PCA covariates for methylation ---
-  U_M     <- meth.posER.pc.sig[meth_row]
-  U_meth  <- pca_M_final %>% select(all_of(as.numeric(U_M[[1]])))
+  # Retrieve significant PCs for methylation
+  meth_pc_index <- meth.table.pos %>% filter(col1 == meth_row) %>% pull(col2)
+  U_meth <- pca_M_final %>% select(all_of(as.numeric(meth_indice.pos[meth_pc_index][[1]])))
+  
+  Total.PC.Count <- sum(ncol(U_exp), ncol(U_meth), na.rm = TRUE)
   
   # --- Build regression dataframe ---
   df <- cbind(
@@ -130,16 +138,25 @@ get_trio_data <- function(trio_row,
 # --- a trio example ----
 df_trio_144897 <- get_trio_data(144897, trios, pos_meth_filter, pos_cna_filter,
                                 pos_exp_filter, pca_E_final, pca_M_final,
-                                gene_pos_indice, meth.posER.pc.sig,
+                                gene_indice.pos, meth_indice.pos,gene.table.pos,meth.table.pos,
                                 pos_age, pos_race)
 
 # some error running MGRN, need Define isTrue as it's missing from your environment
- assign("isTrue", isTRUE, envir = .GlobalEnv)
+assign("isTrue", isTRUE, envir = .GlobalEnv)
 
 # --- Use MRGN to find the trio belong to which model ----
 ### Find the model of the trio
 
-model_trio_144897 <- infer.trio(df_trio_144897,is.CNA = TRUE,compute.nominal=TRUE)
+model_trio_144897_with_U <- infer.trio(df_trio_144897,is.CNA = TRUE,compute.nominal=TRUE)
+
+model_trio_144897_without_U  <- infer.trio(df_trio_144897[1:3],is.CNA = TRUE,compute.nominal=TRUE)
+
+### with Confounder, Inferred to M1.2
+print(model_trio_144897_with_U)
+
+### without Confounder, Inferred to M4
+print(model_trio_144897_without_U)
+
 
 
 
