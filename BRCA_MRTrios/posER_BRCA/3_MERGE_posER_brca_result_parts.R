cdd6e6b46491d0db618f46eb93e6208ffee4d351
LianZuo <112709486+LianZuo8197@users.noreply.github.com>
2026-08-09 19:36:24 -0400

    Update 3_MERGE_posER_brca_result_parts.R

--- BRCA_MRTrios/posER_BRCA/3_MERGE_posER_brca_result_parts.R
+++ BRCA_MRTrios/posER_BRCA/3_MERGE_posER_brca_result_parts.R
@@ -1,7 +1,12 @@
 library(data.table)
 
-input_dir <- "/wsu/home/hb/hb68/hb6890/fulab/MRTrios/Output_posER_BRCA_part"
-output_dir <- "/wsu/home/hb/hb68/hb6890/fulab/MRTrios/Output_posER_BRCA"
+## Ondemand path:
+# input_dir <- "/wsu/home/hb/hb68/hb6890/fulab/MRTrios/Output_posER_BRCA_part"
+# output_dir <- "/wsu/home/hb/hb68/hb6890/fulab/MRTrios/Output_posER_BRCA"
+
+## Local path:
+input_dir <- "/Users/lianzuo/LZ/ResearchProject/Fulab/MRTrios_BRCA/Output_posER_BRCA_part"
+output_dir <- "/Users/lianzuo/LZ/ResearchProject/Fulab/MRTrios_BRCA/Output_posER_BRCA"
 
 # ── Find all shard files (exclude merged ALL file) ─────────
 shard_files <- sort(list.files(input_dir,
@@ -9,6 +14,7 @@ shard_files <- sort(list.files(input_dir,
                                full.names = TRUE))
 
 cat(sprintf("Found %d shard files. Merging...\n", length(shard_files)))
+##  600 shard files
 
 # ── Merge all shards ───────────────────────────────────────
 all_results <- rbindlist(lapply(shard_files, fread), fill = TRUE)
@@ -16,22 +22,33 @@ all_results <- rbindlist(lapply(shard_files, fread), fill = TRUE)
 cat(sprintf("Total rows: %d | Total cols: %d\n",
             nrow(all_results), ncol(all_results)))
 
+## Total rows: 292525 | Total cols: 24
+
 # ── Save merged file ───────────────────────────────────────
 out_file <- file.path(output_dir, "trio_posER_BRCA_results_ALL.txt")
 fwrite(all_results, file = out_file, sep = "\t")
 
 cat(sprintf("Saved: %s\n", out_file))
 
+## Saved: /Users/lianzuo/LZ/ResearchProject/Fulab/MRTrios_BRCA/Output_posER_BRCA/trio_posER_BRCA_results_ALL.txt
 
-# ==========================================================
+# ====================================================================================================
 ## Merge info:
-# # 08/07/26 Fri: Ondemand done with
-# array 10: 1-58
-# array 1/2/6/7:1-56
-# array 5/8: 1-54
-# array 4: 1-52
-# array 3 : 1-55
-# ==== Merge above parts:
-## 496 shard files:
-## Total rows: 245899 | Total cols: 24
-# ==========================================================
+# # 08/04/26 Fri - 8/9/26 Sun : Ondemand done with
+# 1-10 array: 1-60 parts
+## Merge above parts:
+# 600 shard files:
+## Total rows: 292525 | Total cols: 24
+
+# colnames(all_results)
+# [1] "trio_row"       "meth.row"       "cna.row"        "gene.row"       "Total.PC.Count"
+# [6] "Confounders"    "b11"            "b12"            "b21"            "b22"           
+#[11] "V1:T1"          "V1:T2"          "pb11"           "pb12"           "pb21"          
+#[16] "pb22"           "pV1:T1"         "pV1:T2"         "Minor.freq"     "coef11"        
+#[21] "coef12"         "coef21"         "coef22"         "Inferred.Model"
+
+# table(all_results$Confounders)
+
+# age|race|U_exp|U_meth       age|race|U_meth 
+#               292372                   153 
+# ====================================================================================================
